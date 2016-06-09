43ddbf9974d243a9d3ef27668400bc366670d6e4
Heather Park <hthr.prk@gmail.com>
2016-06-09 14:03:26 -0700

    Write stack function for functional-shared method

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,6 +1,11 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newInstance = {};
+  newInstance.storage = {};
+  newInstance.index = 0;
+
+  return newInstance;
 };
 
 var queueMethods = {};
