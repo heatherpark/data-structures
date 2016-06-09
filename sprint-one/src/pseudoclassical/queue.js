627576984f76a7e7b805c1d3bf641638f7ff95d9
Heather Park <hthr.prk@gmail.com>
2016-06-09 15:27:14 -0700

    Write queue function for pseudoclassical method

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.index = 0;
+};
+
+var newInstance = new Queue();
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.index] = value;
+  this.index++;
+};
+
+Queue.prototype.dequeue = function() {
+  if (this.index !== 0) {
+    var first = this.storage[0];
+    delete this.storage[0];
+    this.index--;
+    for (var key in this.storage) {
+      this.storage[Number(key) - 1] = this.storage[key];
+      delete this.storage[key];
+    }
+    return first;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.index;
 };
 
 
