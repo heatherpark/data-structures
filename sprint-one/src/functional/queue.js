fc145ddf786c4c892e4631c713f956db1ded1568
Heather Park <hthr.prk@gmail.com>
2016-06-09 11:41:29 -0700

    Write stack and queue functions for functional method

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var index = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[index] = value;
+    index++;
   };
 
   someInstance.dequeue = function() {
+    if (index !== 0) {
+      var firstItem = storage[0];
+      delete storage[0];
+      index--;
+      for (var key in storage) {
+        storage[Number(key) - 1] = storage[key];
+        delete storage[key];
+      }
+      return firstItem;
+    }
   };
 
   someInstance.size = function() {
+    return index;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -5,13 +5,24 @@ var Stack = function() {
   var storage = {};
 
   // Implement the methods below
+  var topIndex = 0;
+
   someInstance.push = function(value) {
+    storage[topIndex] = value;
+    topIndex += 1;
   };
 
   someInstance.pop = function() {
+    if (topIndex !== 0) {
+      var topItem = storage[topIndex - 1];
+      delete storage[topIndex - 1];
+      topIndex -= 1;
+      return topItem;
+    }
   };
 
   someInstance.size = function() {
+    return topIndex;
   };
 
   return someInstance;
