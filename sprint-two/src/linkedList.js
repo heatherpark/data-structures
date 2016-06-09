48292ed98b56c7c8b040ba8966643e92205308a6
Heather Park <hthr.prk@gmail.com>
2016-06-09 16:28:12 -0700

    Write LinkedList function

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,34 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    var formerHead = list.head.value;
+    list.head = list.head.next;
+    return formerHead;
   };
 
   list.contains = function(target) {
+    var currentLink = list.head;
+    while (currentLink.next !== null) {
+      if (currentLink.value === target) {
+        return true;
+      }
+      currentLink = currentLink.next;
+    }
+    if (currentLink.value === target) {
+      return true;
+    }
+    return false;
   };
 
   return list;
@@ -24,6 +46,10 @@ var Node = function(value) {
   return node;
 };
 
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ // addToTail and removeHead are O(1) or constant.
+ // contains is O(n) or linear.
