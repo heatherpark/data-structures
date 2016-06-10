26b94b5a9a7a694509b9ac99e6a273b426f88dba
Heather Park <hthr.prk@gmail.com>
2016-06-10 12:08:22 -0700

    Complete set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,32 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  return (_.indexOf(this._storage, item) > -1) ? true : false;
 };
 
 setPrototype.remove = function(item) {
+  if (this.contains(item)) {
+    var index = _.indexOf(this._storage, item);
+    this._storage.splice(index, 1);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+ // add() is constant
+ // contains() is linear
+ // remove() is quadratic
