03ffd361b81cc09c1bc34528fe78313486d31db4
Heather Park <hthr.prk@gmail.com>
2016-06-10 20:23:42 -0700

    Write binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,61 @@
 var BinarySearchTree = function(value) {
+  var node = Object.create(BinarySearchTree.prototype);
+
+  node.value = value;
+  node.left = null;
+  node.right = null;
+
+  return node;
 };
 
+
+BinarySearchTree.prototype.insert = function(value) {
+  var tree = BinarySearchTree(value);
+
+  if (tree.value < this.value) {
+    if (this.left === null) {
+      this.left = tree;
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = tree;
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(target) {
+  var result = false;
+
+  if (this.value === target) {
+    result = true;
+  } else {
+    if (target > this.value) {
+      if (this.right !== null) {
+        result = this.right.contains(target);
+      }
+    } else {
+      if (this.left !== null) {
+        result = this.left.contains(target);
+      }
+    }
+  }
+
+  return result;
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  callback(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
